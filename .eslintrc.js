938b8fe2bbf6e40aacb87ec6270a87d9fba42f82
vcttai <vcttai@gmail.com>
2020-04-27 21:48:30 +0700

    Init the project

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    "semi": ["error", "always", { "omitLastInOneLineBlock": true}],
+    "space-before-function-paren": ["error", "never"],
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "never",
+      "exports": "never",
+  }],
+  }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# circleci-jest-eslint
+
+This project is used as a demo for:
+- Adding unit test (jest) to the Javascript project
+- Adding ESlint to lint the code
+- Configuring a repository with CircleCI to perform continuous-integration
+
+More info:
+- Event & Presentation file: [Introduction to CI/CD with CircleCI](https://docs.google.com/presentation/d/12RTWRp6yW4o-cTp_sW3tHJviF451VAqcW3lJE_hlkBQ/edit)
+- Presentor: Vu Cong Tan Tai
+- Email: vcttai@gmail.com
+- Website: https://nhungdongcodevui.com/ 
